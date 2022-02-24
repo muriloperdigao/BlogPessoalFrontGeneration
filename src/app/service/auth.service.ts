bf6d64b03fedddf0c0d68fd4720936743abb4844
muriloperdigao <murilo.apm@gmail.com>
2022-02-24 17:02:33 -0300

    Primeiro Up Componente 24.02.2022

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 
@@ -19,5 +20,15 @@ export class AuthService {
 
   cadastrar(user: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>("http://localhost:8080/usuario/cadastrar", user)
+  }
+
+  logado(){
+    let ok = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 }
