b9a18327d4a2630a35a4410d33758467a11ad3ed
muriloperdigao <murilo.apm@gmail.com>
2022-03-03 21:23:41 -0300

    Implementação de get e post Tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,16 +11,35 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
   constructor(
-    private router:Router
+    private router:Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
     if(environment.token == ''){
       alert('Sua sessão expirou. Faça o login novamente.')
-      // caso o token esteja vazio pela atualização da página o usuário será direcionado à página de entrar pela rota
       this.router.navigate(['/entrar'])
     }
+    this.findAllTemas()
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=> {
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
   }
 
 }
