7953a53f9cf6c60b88f32294d5a56f433ea44574
muriloperdigao <murilo.apm@gmail.com>
2022-03-07 02:53:51 -0300

    Implementação de put e delete Postagem

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
 
 @Injectable({
   providedIn: 'root'
@@ -19,8 +20,28 @@ export class PostagemService {
     return this.http.get<Postagem[]>('http://localhost:8080/postagens', this.token)
   }
 
+  getAllTema(): Observable<Tema[]> {
+    return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
+  }
+
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`http://localhost:8080/postagens/${id}`, this.token)
+  }
+
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`http://localhost:8080/postagens/${id}`, this.token)
   }
 
 }
